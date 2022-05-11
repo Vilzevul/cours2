5ae8e94a256a3cc8e02bfa3a6d40b8784f3fbd60
AlbusCat <olegkveko@gmail.com>
2022-05-11 15:25:29 +0700

    Initial commit

---
+++ src/test/java/pro/sky/cours2/Cours2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cours2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
